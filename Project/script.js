d6bcb150bc52f1c4f2a676463de96c1cac385d9f
fabio correa <fffjv2006@gmail.com>
2023-10-29 23:03:33 -0300

    I just finish the site with api

---
+++ Project/script.js
@@ -0,0 +1,73 @@
+const download = document.querySelector("#download-button");
+const shareBtn = document.querySelector("#share-button");
+const qrContainer = document.querySelector("#qr-code");
+const qrText = document.querySelector("#conteudo");
+const sizes = document.querySelector(".main__select");
+
+const config = {
+  colorLight: "#fff",
+  colorDark: "#000",
+  text: "Hello, world!",
+  size: 300
+};
+
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change", handleSize);
+shareBtn.addEventListener("click", handleShare);
+
+function handleQRText(e) {
+    const value = e.target.value;
+    config.text = value || "Hello, world!";
+    generateQRCode();
+}
+
+async function generateQRCode() {
+    qrContainer.innerHTML = "";
+    new QRCode("qr-code", {
+        text: config.text,
+        height: config.size,
+        width: config.size,
+        colorLight: config.colorLight,
+        colorDark: config.colorDark,
+    });
+    download.href = await resolveDataUrl();
+}
+
+async function handleShare() {
+    setTimeout(async () => {
+        try {
+            const base64url = await resolveDataUrl();
+            const blob = await (await fetch(base64url)).blob();
+            const file = new File([blob], "QRCode.png", {
+                type: blob.type,
+            });
+            await navigator.share({
+                files: [file],
+                title: config.text,
+            });
+        } catch (error) {
+            alert("Your browser doesn't support sharing or an error occurred.");
+        }
+    }, 100);
+}
+
+function handleSize(e) {
+    config.size = e.target.value;
+    generateQRCode();
+}
+
+function resolveDataUrl() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const img = document.querySelector("#qr-code img");
+            if (img.currentSrc) {
+                resolve(img.currentSrc);
+                return;
+            }
+            const canvas = document.querySelector("canvas");
+            resolve(canvas.toDataURL());
+        }, 50);
+    });
+}
+
+generateQRCode();
